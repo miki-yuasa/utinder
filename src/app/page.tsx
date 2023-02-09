3346476234256c2696015de756e2de5f08f6a1d4
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-02-09 16:01:48 -0600

    feat: responsive grid added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { Inter, Roboto_Flex } from '@next/font/google'
 import styles from './page.module.css'
 
 import { Button } from '@/components/Button/Button'
+import { Grid } from '@/components/Grid/Grid'
+import { Box } from '@mui/material'
 
 const inter = Roboto_Flex({ subsets: ["latin"] })// Inter({ subsets: ['latin'] })
 
@@ -26,47 +28,24 @@ export default function Home() {
       <div className={styles.center}>
         <Button variant='text'>教科を探す</Button>
       </div>
-
       <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            評価の管理・編集
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            匿名で評価を投稿
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            投稿を評価
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <Grid container spacing={10} style={{ textAlign: "center" }}>
+          <Grid item xs={12} md={4}  >
+            <h2 className={inter.className}>
+              評価の管理・編集
+            </h2>
+          </Grid>
+          <Grid item xs={12} md={4} >
+            <h2 className={inter.className}>
+              匿名で評価を投稿
+            </h2>
+          </Grid>
+          <Grid item xs={12} md={4} >
+            <h2 className={inter.className}>
+              投稿を評価
+            </h2>
+          </Grid>
+        </Grid>
       </div>
     </main>
   )
