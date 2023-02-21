d66898244e15024a0853515ee67aecc10f44742f
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-02-21 01:40:29 -0600

    feat: uni name automatically appears

--- src/app/[uni]/page.tsx
+++ src/app/[uni]/page.tsx
@@ -5,17 +5,23 @@ import { Footer } from "@/components/Footer/Footer";
 import { Typography } from "@/components/Typography/Typography";
 import { Navigation } from "@/features/navigation";
 
-
+type Option = {
+    id: number
+    label: string
+    slug: string
+}
 
 export default function Page({ params }: { params: { slug: string } }) {
     const { slug } = params;
-
+    const options = getUniversity()
+    const uni_slug: string = window.location.pathname.replace('/', '')
+    const uni_name: string = options.find(option => option.slug === uni_slug)?.label!
     return (<>
         <Navigation />
         <main style={{ minHeight: '50vh' }}>
             <Container maxWidth='lg'>
                 <Box marginTop={30}>
-                    <Typography variant="h3" align="center"> <b>東京大学</b>の授業評定を見る</Typography>
+                    <Typography variant="h3" align="center"><b>{uni_name}</b>の授業評定を見る</Typography>
                 </Box>
             </Container>
         </main>
@@ -27,10 +33,11 @@ export async function generateStaticParams() {
     const unis = await getUniversity();
 
     return unis.map((uni) => ({
-        slug: uni.name,
+        slug: uni.slug,
     }));
 }
 
 const getUniversity = () => {
-    return ([{ name: 'utokyo' }, { name: 'keio' }])
+    const options: Option[] = [{ id: 1, label: '東京大学', slug: 'utokyo' }, { id: 2, label: '慶應義塾大学', slug: 'keio' }]
+    return options
 }
