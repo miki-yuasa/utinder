64b9c5ccf3f1f2be9601a9a86cdc4b514aaf2d50
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-02-20 22:03:38 -0600

    ref: layout

--- src/features/navigation/components/Navigation.tsx
+++ src/features/navigation/components/Navigation.tsx
@@ -4,15 +4,18 @@ import { Box } from "@/components/Box/Box"
 import { Toolbar } from "@/components/Toolbar/Toolbar"
 import { Typography } from "@/components/Typography/Typography"
 import { Container } from "@/components/Container/Container"
+import { Stack } from "@/components/Stack/Stack"
 
-export const Navigation = () => {
+export const Navigation = ({ paddingTop = 12 }: { paddingTop?: number }) => {
     return (
-        <AppBar position="static" color="transparent" sx={{ backdropFilter: "blur(20px)", paddingTop: 6 }}>
+        <AppBar elevation={0} position="static" color="transparent" sx={{ backdropFilter: "blur(20px)", paddingTop: paddingTop }}>
             <Container maxWidth='lg'>
                 <Toolbar>
-                    <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>UTinder</Typography>
-                    <Button variant='text' color='inherit'>ログイン</Button>
-                    <Button variant='contained'>サインアップ</Button>
+                    <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>UTinder</Typography>
+                    <Stack spacing={2} direction='row'>
+                        <Button variant='text' color='inherit'>ログイン</Button>
+                        <Button variant='contained'>サインアップ</Button>
+                    </Stack>
                 </Toolbar>
             </Container>
         </AppBar>
