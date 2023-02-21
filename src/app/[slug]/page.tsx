f4eddb948c0879f34f208db4d628cc17526a5c5a
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-02-20 22:04:01 -0600

    feat: page added

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,11 +1,21 @@
 'use client'
+import { Box } from "@/components/Box/Box";
+import { Container } from "@/components/Container/Container";
+import { Typography } from "@/components/Typography/Typography";
 import { Navigation } from "@/features/navigation";
 
 
 export default function Page({ params }: { params: { slug: string } }) {
     const { slug } = params;
 
-    return (<main><Navigation /></main>)
+    return (<>
+        <Navigation />
+        <Container maxWidth='lg'>
+            <Box marginTop={30}>
+                <Typography variant="h3" align="center"> <b>東京大学</b>の授業評定を見る</Typography>
+            </Box>
+        </Container>
+    </>)
 }
 
 export async function generateStaticParams() {
