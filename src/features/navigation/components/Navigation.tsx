d4a77d56842b90cc717350522c85f444a5dd8cea
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-02-20 21:25:19 -0600

    feat: navigation added

---
+++ src/features/navigation/components/Navigation.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/Button/Button"
+import { AppBar } from "@/components/AppBar/AppBar"
+import { Box } from "@/components/Box/Box"
+import { Toolbar } from "@/components/Toolbar/Toolbar"
+import { Typography } from "@/components/Typography/Typography"
+import { Container } from "@/components/Container/Container"
+
+export const Navigation = () => {
+    return (
+        <AppBar position="static" color="transparent" sx={{ backdropFilter: "blur(20px)" }}>
+            <Container maxWidth='lg'>
+                <Toolbar>
+                    <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>UTinder</Typography>
+                    <Button variant='text' color='inherit'>ログイン</Button>
+                    <Button variant='contained'>サインアップ</Button>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
+
+}
