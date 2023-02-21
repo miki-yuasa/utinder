e1d420d65e9a47446a5294220f26de097ff2e9a4
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-02-21 13:12:00 -0600

    feat: college selection

--- src/app/[uni]/page.tsx
+++ src/app/[uni]/page.tsx
@@ -1,7 +1,9 @@
 'use client'
+import { Autocomplete } from "@/components/Autocomplete/Autocomplete";
 import { Box } from "@/components/Box/Box";
 import { Container } from "@/components/Container/Container";
 import { Footer } from "@/components/Footer/Footer";
+import { TextField, TextFieldProps } from "@/components/TextField/TextField";
 import { Typography } from "@/components/Typography/Typography";
 import { Navigation } from "@/features/navigation";
 import { usePathname } from 'next/navigation';
@@ -12,13 +14,23 @@ export default function Page({ params }: { params: { slug: string } }) {
 
     const { slug } = params;
     const options = getUniversity()
-    const uni_name: string = options.find(option => option.slug === uni_slug)?.label!
+    const uni = options.find(option => option.slug === uni_slug)!
+    const uni_name: string = uni.label
     return (<>
         <Navigation />
         <main style={{ minHeight: '50vh' }}>
             <Container maxWidth='lg'>
                 <Box marginTop={30}>
                     <Typography variant="h3" align="center"><b>{uni_name}</b>の授業評定を見る</Typography>
+                </Box>
+                <Box justifyContent={'center'} display='flex' marginTop={15}>
+                    <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        options={uni.colleges}
+                        sx={{ width: 300 }}
+                        renderInput={(params: TextFieldProps) => <TextField {...params} label="学部名" />}
+                    />
                 </Box>
             </Container>
         </main>
@@ -33,12 +45,17 @@ export async function generateStaticParams() {
         slug: uni.slug,
     }));
 }
+
 type Option = {
     id: number
     label: string
     slug: string
+    colleges: string[]
 }
 const getUniversity = () => {
-    const options: Option[] = [{ id: 1, label: '東京大学', slug: 'utokyo' }, { id: 2, label: '慶應義塾大学', slug: 'keio' }]
+    const options: Option[] = [
+        { id: 1, label: '東京大学', slug: 'utokyo', colleges: ['理学部', '工学部'] },
+        { id: 2, label: '慶應義塾大学', slug: 'keio', colleges: ['理工学部', '経済学部'] }
+    ]
     return options
 }
