c941c1eb39e4d1e4d3767c924daf7bf2d2dc5cc7
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-02-20 22:04:12 -0600

    ref: ui components now in MUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ import { Grid } from '@/components/Grid/Grid'
 import { Stack } from '@/components/Stack/Stack'
 import { Autocomplete } from '@/components/Autocomplete/Autocomplete'
 import { TextField, TextFieldProps } from '@/components/TextField/TextField'
+import { Navigation } from '@/features/navigation'
+import { Container } from '@/components/Container/Container'
+import { Box } from '@/components/Box/Box'
+import { Typography } from '@/components/Typography/Typography'
 
 const options = [{ id: 1, label: '東京大学' }, { id: 2, label: '慶應義塾大学' }]
 
@@ -18,48 +22,41 @@ const inter = Noto_Sans_JP({
 })
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <h1 className={inter.className}>
-          UTinder
-        </h1>
-        <Stack spacing={2} direction='row'>
-          <Button variant='text' color='inherit'>ログイン</Button>
-          <Button variant='contained'>サインアップ</Button>
-        </Stack>
-      </div>
-
-      <div className={styles.center}>
-        <h1 className={inter.className}><b>大学</b>を探す</h1>
-      </div>
-      <div className={styles.center}>
-        <Autocomplete
-          disablePortal
-          id="combo-box-demo"
-          options={options}
-          sx={{ width: 300 }}
-          renderInput={(params: TextFieldProps) => <TextField {...params} label="大学名" />}
-        />
-      </div>
-      <div className={styles.grid}>
-        <Grid container spacing={10} style={{ textAlign: "center" }}>
-          <Grid item xs={12} md={4}  >
-            <h2 className={inter.className}>
-              評価の管理・編集
-            </h2>
+    <main >
+      <Navigation />
+      <Container maxWidth='lg'>
+        <Box marginTop={30}>
+          <Typography variant="h3" align="center"> <b>大学</b>を探す</Typography>
+        </Box>
+        <Box justifyContent={'center'} display='flex' marginTop={15}>
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={options}
+            sx={{ width: 300 }}
+            renderInput={(params: TextFieldProps) => <TextField {...params} label="大学名" />}
+          />
+        </Box>
+        <Box justifyContent={'center'} display='flex' marginTop={15}>
+          <Grid container spacing={10} style={{ textAlign: "center" }}>
+            <Grid item xs={12} md={4}  >
+              <h2 className={inter.className}>
+                評価の管理・編集
+              </h2>
+            </Grid>
+            <Grid item xs={12} md={4} >
+              <h2 className={inter.className}>
+                匿名で評価を投稿
+              </h2>
+            </Grid>
+            <Grid item xs={12} md={4} >
+              <h2 className={inter.className}>
+                投稿を評価
+              </h2>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={4} >
-            <h2 className={inter.className}>
-              匿名で評価を投稿
-            </h2>
-          </Grid>
-          <Grid item xs={12} md={4} >
-            <h2 className={inter.className}>
-              投稿を評価
-            </h2>
-          </Grid>
-        </Grid>
-      </div>
+        </Box>
+      </Container>
     </main>
   )
 }
