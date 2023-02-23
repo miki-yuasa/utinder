ef0820cd508f535f268bc8c77597fa0f681739e2
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-02-23 12:34:48 -0600

    feat: database config added

---
+++ src/lib/database.tsx
@@ -0,0 +1,29 @@
+import { initializeApp, FirebaseOptions } from "firebase/app"
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore/lite'
+
+import {
+    FIREBASE_DATABASE_URL,
+    FIREBASE_API_KEY,
+    FIREBASE_APP_ID,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_MEASUREMENT_ID,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET
+} from "@/config"
+
+const firebaseConfig: FirebaseOptions = {
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    databaseURL: FIREBASE_DATABASE_URL,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const db = getFirestore(app)
